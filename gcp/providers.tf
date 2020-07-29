321d5a46f0987aec4f413a5b0481ad4ce1a2c2ee
Yu Fu <yu.fu3@anz.com>
2020-07-29 17:02:49 +1000

    add terraform backend

--- gcp/providers.tf
+++ gcp/providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "devops-training"
+
+    workspaces {
+      name = "test-rob"
+    }
+  }
+}
+
 provider "google" {
   project = var.project_id
   region  = var.region
